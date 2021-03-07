dcaabab5322944f1e2627a925c8e68b649bf3eda
Newa <chezsick@orange.fr>
2021-03-07 23:45:38 +0100

    Fix It

--- web/fonts.php
+++ web/fonts.php
@@ -6,10 +6,10 @@ define("SITE_URL",rtrim("http://".$_SERVER["HTTP_HOST"].BASE_FOLDER,"/"));
 define("SITE_HOST",parse_url(SITE_URL,PHP_URL_HOST));
 if ((isset($_SESSION["font_access_key"]) && isset($_SESSION["font_access_token"]) && isset($_GET["font"]) && isset($_GET[$_SESSION["font_access_key"]]) && (isset($_SERVER["HTTP_REFERER"])&& $_GET[$_SESSION["font_access_key"]] == $_SESSION["font_access_token"] && parse_url($_SERVER["HTTP_REFERER"],PHP_URL_HOST) == SITE_HOST)) || (isset($_SESSION["font_access_key_sec"]) && isset($_SESSION["font_access_token_sec"]) && isset($_GET["font"]) && isset($_GET[$_SESSION["font_access_key_sec"]]) && isset($_SERVER["HTTP_REFERER"])&& $_GET[$_SESSION["font_access_key_sec"]] == $_SESSION["font_access_token_sec"] && parse_url($_SERVER["HTTP_REFERER"],PHP_URL_HOST) == SITE_HOST))
 {
-if (file_exists(SITE_ROOT."/fonts/".basename($_GET['font'])))
+if (file_exists("../fonts/".basename($_GET['font'])))
 {
 header("Content-Type:application/octet-stream");
-readfile(SITE_ROOT."/fonts/".basename($_GET['font']));
+readfile("../fonts/".basename($_GET['font']));
 }
 else
 {
