69f9ead0cf92089e3292f9fc20ffafd04e8d8866
Newa <chezsick@orange.fr>
2021-03-08 21:06:40 +0100

    Correction

--- web/fonts.php
+++ web/fonts.php
@@ -4,7 +4,7 @@ define("BASE_FOLDER",dirname($_SERVER["SCRIPT_NAME"]));
 define("SITE_ROOT",pathinfo($_SERVER["SCRIPT_FILENAME"],PATHINFO_DIRNAME));
 define("SITE_URL",rtrim("http://".$_SERVER["HTTP_HOST"].BASE_FOLDER,"/"));
 define("SITE_HOST",parse_url(SITE_URL,PHP_URL_HOST));
-if ((isset($_SESSION["font_access_key"]) && isset($_SESSION["font_access_token"]) && isset($_GET["font"]) && isset($_GET[$_SESSION["font_access_key"]]) && (isset($_SERVER["HTTP_REFERER"])&& $_GET[$_SESSION["font_access_key"]] == $_SESSION["font_access_token"] && parse_url($_SERVER["HTTP_REFERER"],PHP_URL_HOST) == SITE_HOST)) || (isset($_SESSION["font_access_key_sec"]) && isset($_SESSION["font_access_token_sec"]) && isset($_GET["font"]) && isset($_GET[$_SESSION["font_access_key_sec"]]) && isset($_SERVER["HTTP_REFERER"])&& $_GET[$_SESSION["font_access_key_sec"]] == $_SESSION["font_access_token_sec"] && parse_url($_SERVER["HTTP_REFERER"],PHP_URL_HOST) == SITE_HOST))
+if ((isset($_SESSION["font_access_key"]) && isset($_SESSION["font_access_token"]) && isset($_GET["font"]) && isset($_GET[$_SESSION["font_access_key"]]) && (isset($_SERVER["HTTP_REFERER"])&& $_GET[$_SESSION["font_access_key"]] == $_SESSION["font_access_token"] && parse_url($_SERVER["HTTP_REFERER"],PHP_URL_HOST) == SITE_HOST)) || ((isset($_SESSION["font_access_key_sec"]) && isset($_SESSION["font_access_token_sec"]) && isset($_GET["font"]) && isset($_GET[$_SESSION["font_access_key_sec"]]) && isset($_SERVER["HTTP_REFERER"])&& $_GET[$_SESSION["font_access_key_sec"]] == $_SESSION["font_access_token_sec"] && parse_url($_SERVER["HTTP_REFERER"],PHP_URL_HOST) == SITE_HOST)))
 {
 if (file_exists("../fonts/".basename($_GET['font'])))
 {
--- web/index.php
+++ web/index.php
@@ -13,5 +13,5 @@ echo "h1{font-family:\"Oi\";font-weight:bold;}";
 echo "@font-face{font-family:\"DotGothic16\";src:url(\"fonts.php?font=SecondaryFont.ttf&".$_SESSION["font_access_key_sec"]."=".$_SESSION["font_access_token_sec"]."\");}";
 echo "body{font-family:\"DotGothic16\";}</style>";
 echo "<h1>Font Example, Et Essayez De M'attraper ...</h1>";
-echo "La Police D'ecriture Telle Que Vous Le Voyez, N'est Accissble Par La Charge De La Page, Hors Du Charge, Ce Sera Refusé.<br/>";
+echo "La Police D'ecriture Telle Que Vous Le Voyez, N'est Accessible Par La Charge De La Page, Hors Du Charge, Ce Sera Refusé.<br/>";
 echo "Cette Protection De Police Est Basé Sur <a href=\"https://stackoverflow.com/questions/21470085/how-to-protect-webfonts\">Cette Question</a><br/>Bien Que Pour L'exemple Pas Question Utiliser Une Police Sous License/Payante, J'utilise Celui De Google Fonts.";
